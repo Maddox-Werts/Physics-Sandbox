94639d0f166369c4a7bba5849e243e300c0aa8bc
Maddox-Werts <maddoxwerts@gmail.com>
2022-03-01 17:50:29 -0500

    Initial Commit

---
+++ PhysSandbox/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PhysSandbox
+//
+//  Created by Maddox Werts on 3/1/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
